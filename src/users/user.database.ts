8df2d95b842168dd853f996fa59bec1b7c186ab1
unknown <YY208@ccs.local>
2025-02-19 04:33:16 -0800

    Updated database user

--- src/users/user.database.ts
+++ src/users/user.database.ts
@@ -30,7 +30,7 @@ export const findOne = async (id: string): Promise<UnitUser> => users[id];
 
 export const create = async (userData: UnitUser) : Promise < UnitUser | null > => {
 
-    let id =random();
+    let id = random();
    
     let check_user = await findOne(id);
 
@@ -78,4 +78,40 @@ export const comparePassword = async(email : string, supplied_password : string)
         return null
     }
     return user
+};
+
+export const update = async (id: string, updateValues : User) : Promise<UnitUser|null> => {
+    const userExists = await findOne(id)
+
+    if (!userExists) {
+        return null
+    }
+
+    if (updateValues.password) {
+        const salt = await bcrypt.genSalt(10)
+        const newPass = await bcrypt.hash(updateValues.password, salt)
+
+        updateValues.password = newPass
+    }
+
+    users[id] = {
+        ...userExists,
+        ...updateValues
+    }
+    saveUsers()
+
+    return users[id]
+};
+
+export const remove = async (id : string) : Promise<null | void> => {
+
+    const user = await findOne(id)
+
+    if (!user) {
+        return null
+    }
+
+    delete users[id]
+
+    saveUsers()
 }
